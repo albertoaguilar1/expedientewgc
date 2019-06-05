ebce3efb7d9f521440748e0c3345af04a263695a
albertoaguilar1 <faraond1@gmail.com>
2019-06-05 11:44:21 -0500

    se incluye get y set lombok

--- ExpedienteWebgc/src/main/java/expedientewebgc/com/cliente/msj/Mensaje.java
+++ ExpedienteWebgc/src/main/java/expedientewebgc/com/cliente/msj/Mensaje.java
@@ -7,28 +7,14 @@ import expedientewebgc.chedraui.com.cliente.Cliente;
 import lombok.Getter;
 import lombok.Setter;
 
-
+@Getter @Setter
 public class Mensaje {
 		
 	private String atributoBusqueda;
 	
 	private Cliente cliente;
 
-	public String getAtributoBusqueda() {
-		return atributoBusqueda;
-	}
 
-	public void setAtributoBusqueda(String atributoBusqueda) {
-		this.atributoBusqueda = atributoBusqueda;
-	}
-
-	public Cliente getCliente() {
-		return cliente;
-	}
-
-	public void setCliente(Cliente cliente) {
-		this.cliente = cliente;
-	}
 
 	@Override
 	public String toString() {
